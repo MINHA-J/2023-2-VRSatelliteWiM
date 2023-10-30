af46cfb7a12fa17e204f9350ce1a0c53db3c8df5
MINHA-J <harororo721@khu.ac.kr>
2023-10-30 14:56:16 +0900

    [FIX] Set coordinate

--- Assets/Scripts/MiniatureManipulation.cs
+++ Assets/Scripts/MiniatureManipulation.cs
@@ -74,18 +74,37 @@ public class MiniatureManipulation : MonoBehaviour
 
     public void SetOnHand()
     {
-        this.transform.position = hand.PalmPosition;
+        //this.transform.position = hand.PalmPosition;
+        //this.transform.position += this.transform.up * 0.1f;
         
-        directionX = (middle.Bone(Bone.BoneType.TYPE_DISTAL).NextJoint - hand.PalmPosition).normalized;
-        directionY = (thumb.Bone(Bone.BoneType.TYPE_DISTAL).NextJoint - hand.PalmPosition).normalized;
-        directionZ = (index.Bone(Bone.BoneType.TYPE_DISTAL).NextJoint - hand.PalmPosition).normalized;
+        Vector3 miniRight = new Vector3(0,0,0);
+        Vector3 miniUp = new Vector3(0,0,0);
+        Vector3 miniForward = new Vector3(0,0,0);
         
-        this.transform.up = directionY;
-        this.transform.forward = directionZ;
-        this.transform.right = directionX;
+        // Correct coordinate
+        
+        Plane perpendicularPlane = new Plane(directionY, this.transform.position);
 
-        this.transform.position += this.transform.up * 0.1f;
+        directionZ *= Mathf.Acos(Vector3.Dot(directionY, directionZ) / directionY.magnitude / directionZ.magnitude) * Mathf.Rad2Deg;
+
+        miniUp = directionY; // first, set up vector
+        miniForward = (directionZ - directionY).normalized; // set forward vector
+        miniRight = Vector3.Cross(miniUp, miniForward).normalized; // set right vector
+
+        // Set coordinate
+        Debug.DrawLine(transform.position, transform.position + miniUp * 0.5f, Color.green, 0.01f, false);
+        Debug.DrawLine(transform.position, transform.position + miniForward * 0.5f, Color.blue, 0.01f, false);
+        Debug.DrawLine(transform.position, transform.position + miniRight * 0.5f, Color.red, 0.01f, false);
+        
+        this.transform.right = miniRight;
+        this.transform.up = miniUp;
+        this.transform.forward = miniForward;
+
+        Debug.DrawLine(transform.position, transform.position + transform.up * 0.1f, Color.green, 0.01f, false);
+        Debug.DrawLine(transform.position, transform.position + transform.forward * 0.1f, Color.blue, 0.01f, false);
+        Debug.DrawLine(transform.position, transform.position + transform.right * 0.1f, Color.red, 0.01f, false);
     }
+    
     
 #if UNITY_EDITOR
      private void OnDrawGizmos()
