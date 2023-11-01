d2bd52a82b2d43da34313302990d814afc075652
MINHA-J <64896960+MINHA-J@users.noreply.github.com>
2023-11-01 19:13:21 +0900

    [ADD] head distance to ratio

--- Assets/Scripts/MiniatureManipulation.cs
+++ Assets/Scripts/MiniatureManipulation.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Leap;
 using UnityEngine;
 using Leap.Unity;
 using Leap.Unity.Attributes;
+using Leap.Unity.Interaction;
+using Unity.XR.CoreUtils;
 
 public class MiniatureManipulation : MonoBehaviour
 {
@@ -11,13 +14,33 @@ public class MiniatureManipulation : MonoBehaviour
     private bool ShowGizmos = true;
 
     public HandModelBase HandModel;
+    public GameObject HeadModel;
+    
     private Transform inWorldTransform;
-
+    private MiniatureWorld miniatureWorld;
+    
+    // hand coordinate
     private Leap.Hand hand;
     private Finger thumb, index, middle;
     private Vector3 directionX, directionY, directionZ;
     private float radius;
+
+    // roi and miniature
+    private GameObject worldROI;
+    private Vector3 worldOriginScale;
+    private Vector3 firstGetPos;
+    private bool SetfirstPos = false;
     
+    // grasp
+    private InteractionBehaviour interactionBehaviour;
+
+    private void Awake()
+    {
+        miniatureWorld = GetComponent<MiniatureWorld>();
+        radius = GetComponent<SphereCollider>().radius;
+        interactionBehaviour = GetComponent<InteractionBehaviour>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,8 +50,10 @@ public class MiniatureManipulation : MonoBehaviour
         thumb = hand.Fingers[0];
         index = hand.Fingers[1];
         middle = hand.Fingers[2];
-
-        radius = GetComponent<SphereCollider>().radius;
+        
+        //기존 설정된 ROI의 Size를 set
+        worldROI = miniatureWorld.ROI.gameObject;
+        worldOriginScale = worldROI.transform.localScale;
     }
 
     // Update is called once per frame
@@ -37,10 +62,19 @@ public class MiniatureManipulation : MonoBehaviour
         IsHolding = checkHandCoordinate();
         if (IsHolding)
         {
-            SetOnHand();
+            if (!SetfirstPos)
+            {
+                firstGetPos = hand.PalmPosition;
+                SetfirstPos = true;
+                
+                SetOnHand();
+            }
+            
+            SetRoiRatio();
         }
         else
         {
+            SetfirstPos = false;
             this.transform.position = inWorldTransform.position;
             this.transform.rotation = inWorldTransform.rotation;
         }
@@ -59,24 +93,36 @@ public class MiniatureManipulation : MonoBehaviour
         
         // 내적이 0에 가까운 작은 오차 범위 내에 있는 경우, 두 직선은 직교합니다.
         float epsilon = 0.5f;
-        //if (Mathf.Abs(dotProductXY) < epsilon && Mathf.Abs(dotProductYZ) < epsilon && Mathf.Abs(dotProductZX) < epsilon)
-        if ( Mathf.Abs(dotProductYZ) < epsilon)
-        {
-            Debug.Log("Hand Coordinate ON");
+        if (Mathf.Abs(dotProductXY) < epsilon && Mathf.Abs(dotProductYZ) < epsilon && Mathf.Abs(dotProductZX) < epsilon)
+        //if ( Mathf.Abs(dotProductYZ) < epsilon)
             return true;
-        }
-        else
-        {
-            //Debug.Log("Can't set hand coordinate.");
+        else if (interactionBehaviour.isGrasped)
+            return true;
+        else 
             return false;
-        }
     }
 
+    private void SetRoiRatio()
+    {
+        Vector3 curGetPos = this.transform.position;
+        float ratio = (curGetPos - HeadModel.transform.position).magnitude /
+                      (firstGetPos - HeadModel.transform.position).magnitude;
+
+        if (ratio > 0.93f)
+            ratio = 1.0f;
+        
+        Vector3 ROIscale = Vector3.Lerp(new Vector3(2, 2, 2), worldOriginScale, ratio);
+        worldROI.transform.localScale = ROIscale;
+        
+        Debug.Log(ratio);
+    }
+    
     public void SetOnHand()
     {
-        //this.transform.position = hand.PalmPosition;
-        //this.transform.position += this.transform.up * 0.1f;
+        this.transform.position = hand.PalmPosition;
+        this.transform.position += this.transform.up * 0.1f;
         
+        /* 231101 잠시 test 중 >> 음 이게 낫군...
         Vector3 miniRight = new Vector3(0,0,0);
         Vector3 miniUp = new Vector3(0,0,0);
         Vector3 miniForward = new Vector3(0,0,0);
@@ -103,6 +149,12 @@ public class MiniatureManipulation : MonoBehaviour
         Debug.DrawLine(transform.position, transform.position + transform.up * 0.1f, Color.green, 0.01f, false);
         Debug.DrawLine(transform.position, transform.position + transform.forward * 0.1f, Color.blue, 0.01f, false);
         Debug.DrawLine(transform.position, transform.position + transform.right * 0.1f, Color.red, 0.01f, false);
+        */
+        
+        //this.transform.right = hand.PalmNormal;
+        //this.transform.up = hand.GetThumb().Direction;
+        //this.transform.forward = miniForward;
+        inWorldTransform = this.transform;
     }
     
     
