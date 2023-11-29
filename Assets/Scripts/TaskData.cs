a87d67377c01d06bcd570a63811e100e2e014db8
MINHA-J <64896960+MINHA-J@users.noreply.github.com>
2023-11-29 18:57:17 +0900

    Set test scene

---
+++ Assets/Scripts/TaskData.cs
@@ -0,0 +1,14 @@
+﻿
+    public class TaskData
+    {
+        private uint subjectNum;            //실험자 번호
+
+        private uint testNum;               //실험번호
+
+        private float totalTime;            //실험에 총 걸린 시간
+        private uint portalCreationNum;     //포탈 생성 갯수
+        private float portalCreationTime;   //포탈 생성에 걸린 시간
+        
+        private float movement;             //물리적인 움직임 정도
+
+    }
